e45a7c1b900a1c6f150f76bfce407f6239db956e
TerryDennisonJr <terrysc85@gmail.com>
2022-02-23 18:51:54 -0700

    Re-name Project

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,26 +1,35 @@
- class Calculator{
-	//Declaration of object variables
-	double amount;
-	int years;
-	
-	//Declaration of object with constructors
-	Calculator(double newAmount, int newYear){
-		amount = newAmount;
-		years = newYear;
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) throws Exception {
+        System.out.print( "APY Bank Calculator \n");
+		
+		//Create scanner object
+		Scanner myobj = new Scanner(System.in);
+		
+		System.out.println("\nBank of America: .05%" + "\nNavy Federal: .04%" + "\nChase Bank: .01%");
+		
+		
+		System.out.print( "\nPlease enter deposit amount: $");
+		
+		//Sets users entry of Amount
+		double user_amt = myobj.nextDouble();
+		
+		System.out.print("\nPlease enter years: ");
+		
+		//Sets users entry of Years
+		int user_year = myobj.nextInt();
+		
+		//Create Calc object and sets parameters
+		Calc c = new Calc(user_amt,user_year);
+		
+		//Displays data of calculated methods
+		System.out.printf("\nYour interest earned in Savings with 'Bank of America' after " + user_year + " years would be $" +"%.2f",c.boa_get_APY());
+		System.out.printf("\nYour interest earned in Savings with 'Navy Federal' after " + user_year + " years would be $" +"%.2f",c.navy_get_APY());
+		System.out.printf("\nYour interest earned in Savings with 'Chase Bank' after " + user_year + " years would be $" +"%.2f",c.chase_get_APY());
+
+		//Close scanner object
+		myobj.close();
 	}
-	
-	//Calculates Bank of America APY
-	double boa_get_APY() {
-		return amount * (Math.pow((1+(.005/1)),years));
-	}
-	
-	//Calculates Navy Federal APY
-	double navy_get_APY() {
-		return amount * (Math.pow((1+(.004/1)),years));
-	}
-	
-	//Calculates Chase Bank APY
-	double chase_get_APY() {
-		return amount * (Math.pow((1+(.001/1)),years));
-	}
-}
+
+    }
+
